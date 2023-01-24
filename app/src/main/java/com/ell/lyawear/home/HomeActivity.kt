da0b5981cf72721bfbcb343302db92471b07da42
Elian66 <elian.souza@icomp.ufam.edu.br>
2023-01-23 20:16:10 -0400

    Coleta de dados via celular

--- app/src/main/java/com/ell/lyawear/home/HomeActivity.kt
+++ app/src/main/java/com/ell/lyawear/home/HomeActivity.kt
@@ -1,17 +1,53 @@
 package com.ell.lyawear.home
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.ell.lyawatch.R
 import com.ell.lyawatch.databinding.ActivityHomeBinding
 
-class HomeActivity : AppCompatActivity() {
+
+open class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private var myHandler: Handler? = null
+    var textview: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        textview = findViewById(R.id.text)
+
+        myHandler = Handler { message ->
+            val stuff: Bundle = message.data
+            stuff.getString("messageText")?.let { text -> messageText(text) }
+            return@Handler true
+        }
+
+        val messageFilter = IntentFilter(Intent.ACTION_SEND)
+        val messageReceiver = Receiver()
+        LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, messageFilter)
+    }
+
+    open fun messageText(newInfo: String) {
+        if (newInfo.compareTo("") != 0) {
+            binding.text.append("\n" + newInfo)
+        }
+    }
+}
+
+class Receiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        //Upon receiving each message from the wearable, display the following text//
+        val message = intent?.getStringExtra("message").toString()
+        HomeActivity().textview?.text = message
     }
 }
